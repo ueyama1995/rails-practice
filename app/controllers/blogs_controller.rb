87ea7bedef27d35ba445673c79eee2ebfc9c23a2
ueyama1995 <shinji160829033205@gmail.com>
2022-02-02 05:22:47 +0000

    [update]8章まで復習完了(bookersの再作成は今後実施)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,12 +12,29 @@ class BlogsController < ApplicationController
   end
   
   def create
-    blog = Blog.new(blog_params)
-    blog.save
+    @blog = Blog.new(blog_params)
+    if @blog.save
     redirect_to blog_path(blog.id)
+    else
+     render :new
+    end
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
   
   private
